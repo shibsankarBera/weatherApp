fde3dc0f4a67e327d00a714f597a64557a0e0961
shibsankarBera <shibu900099@gmail.com>
2026-02-26 02:53:30 +0530

    implementing dropdown menu & display and store recently seach cities in local storage

--- weather.js
+++ weather.js
@@ -1,16 +1,22 @@
-// select elements
+// ============================================== SELECT ELEMENTS ==============================================================
 const cityInput = document.getElementById("city_Input"),
   searchBtn = document.getElementById("search_Btn"),
   locationBtn = document.getElementById("locationBtn"),
   api_key = "8930bec60aaae2ab092dff58c1988c4a",
   currentWeatherCard = document.getElementById("currentWeather");
+   let inputDropdownBox=document.querySelector(".inputDropdownBox")
+
+   // ==============================================    STATE/DATA  ============================================================
+  let recentCities = JSON.parse(localStorage.getItem("recentCities")) || [];
 
 
-  ///---------Temparature in farenhite toogle-----
+// ================================================ UTILITIES & HELPER FUNCTIONS =============================================
+///---------Temparature in farenhite toogle-----
 function celcToFahr(n) {
-  return (n * 9.0) / 5.0 + 32.0;
+  return (n * 9.0) / 5.0 + 32.0;  
 }
 
+//-----------------------------------------------feth & displyaying weather Details---------------------
 function getWeatherDetails(name, lat, lon, country, state) {
   let WEATHR_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`,
     FORCAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`,
@@ -95,60 +101,160 @@ function getWeatherDetails(name, lat, lon, country, state) {
     .catch(() => {
       alert(`fail to feth current Weather`);
     });
-   
 }
 
+// =================================================================================================================================
+//=================================================take out city details & coordinates=====================================
 
 
 
 
+searchBtn.addEventListener("click", getcityInput);  ////acces city by search button
 
-
-  ///////// take out city details & coordinates//////////////
-  //  SEARCH BY CITY
-searchBtn.addEventListener("click", () => {
+cityInput.addEventListener("keypress", (e)=> {
+  if(e.key=="Enter"){
+    console.log(e.key)
+    getcityInput();                              ////acess city by press "enter" button
+  }
+});
+///////////////getcity inoromation (Geocoding cityName to cordinate)/////////
+function getcityInput() {
   const city = cityInput.value.trim();
   if (!city) return alert("Enter a city name.");
 
   cityInput.value = "";
 
-  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`)
-    .then(res => res.json())
-    .then(data => {
+  fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${api_key}`,
+  )
+    .then((res) => res.json())
+    .then((data) => {
       if (!data.length) return alert("City not found.");
 
-      const { name, lat, lon, country,state } = data[0];
+      const { name, lat, lon, country, state } = data[0];
       console.log(data);
-      getWeatherDetails(name, lat, lon, country,state);
+      getWeatherDetails(name, lat, lon, country, state);//display weather details
+       saveCity(name);
     })
     .catch(() => alert("Failed to fetch city data."));
-});
+}
 
-
-// USE CURRENT LOCATION
+// ///////////////getcity inoromation by USE CURRENT LOCATION(revers Geocoding city name to cordinate)
 locationBtn.addEventListener("click", () => {
+  if (!navigator.geolocation) return alert("Geolocation not supported.");
 
-  if (!navigator.geolocation)
-    return alert("Geolocation not supported.");
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      const { latitude: lat, longitude: lon } = pos.coords;
 
-  navigator.geolocation.getCurrentPosition(pos => {
+      fetch(
+        `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${api_key}`,
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data.length) return alert("Location not found.");
 
-    const { latitude: lat, longitude: lon } = pos.coords;
-
-    fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${api_key}`)
-      .then(res => res.json())
-      .then(data => {
-
-        if (!data.length) return alert("Location not found.");
-
-        const { name, country,state } = data[0];
-        console.log(data)
-        getWeatherDetails(name, lat, lon, country,state);
-      })
-      .catch(() => alert("Failed to fetch location data."));
-
-  }, () => alert("Location access denied."));
+          const { name, country, state } = data[0];
+          console.log(data);
+          saveCity(name);
+          getWeatherDetails(name, lat, lon, country, state);//display weather details
+         
+        })
+        .catch(() => alert("Failed to fetch location data."));
+    },
+    () => alert("Location access denied."),
+  );
 });
 
 
+
+
+///-----------------------------dropdown menu------------------
+// SHOW dropdown
+
+function showDropdown() {
+  inputDropdownBox.classList.remove("hidden");
+  setTimeout(() => {
+    inputDropdownBox.classList.remove("opacity-0", "scale-y-95");
+    inputDropdownBox.classList.add("opacity-100", "scale-y-100");
+  }, 10);
+}
+
+// HIDE dropdown
+function hideDropdown() {
+  inputDropdownBox.classList.remove("opacity-100", "scale-y-100");
+  inputDropdownBox.classList.add("opacity-0", "scale-y-95");
+
+  setTimeout(() => {
+    inputDropdownBox.classList.add("hidden");
+  }, 200); // match transition duration
+}
+
+
+cityInput.addEventListener("focusin",()=>{
+  if(recentCities.length!==0){
+    showDropdown();
+  }
+} );// Focus in → show
+// Click outside → hide
+document.addEventListener("click", (e) => {
+  if (
+    !cityInput.contains(e.target) &&
+    !inputDropdownBox.contains(e.target)
+  ) {
+    hideDropdown();
+  }
+});
+
+
+
+//load recent from localStorage on dropDownbox
+function renderDropdown() {
+  inputDropdownBox.innerHTML = "";
+ if(!recentCities){
+    hideDropdown();
+  }
+  recentCities.forEach((city) => {
+    const div = document.createElement("div");
+    div.className =
+      "dopDownCard hover:cursor-pointer mb-0.5 px-4 text-center md:px-1 py-1 rounded-xl  bg-indigo-400  text-white";
+
+    div.textContent = city;
+
+    div.addEventListener("click", () => {
+      cityInput.value = city;
+      getcityInput();
+      hideDropdown();
+       console.log(city,":clicked")
+    });
+
+    inputDropdownBox.appendChild(div);
+  });
+}
+
+
+//Save Recent Citys in local storage
+
+
+
+
+
+
+
+function saveCity(city) {
+  // Remove duplicate
+  recentCities = recentCities.filter(c => c.toLowerCase() !== city.toLowerCase());
+
+  // Add to top
+  recentCities.unshift(city);
+
+  // Keep only last 5 cities
+  if (recentCities.length > 5) {
+    recentCities.pop();
+  }
+
+  localStorage.setItem("recentCities", JSON.stringify(recentCities));
+  renderDropdown();
+}
+renderDropdown();
 
